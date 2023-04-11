70068315602e39ba6e8a2b2a21b9010fb34ac2b3
AppleLee0423 <ld210@naver.com>
2023-04-11 15:59:32 +0900

    init commit;

---
+++ src/C_Exam.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : C_Exam.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return 0;
+}
